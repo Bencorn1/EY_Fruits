685b5aaff4bd4b7cc6780d5c0831c60f4f5efd7e
Bencorn1 <moronkejiayotola@gmail.com>
2021-10-08 16:14:02 +0100

    Setting of the view for the fruit details.

---
+++ app/src/main/java/com/ayotola/ey_fruits/Fruit_Details.kt
@@ -0,0 +1,11 @@
+package com.ayotola.ey_fruits
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Fruit_Details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fruit__details)
+    }
+}
